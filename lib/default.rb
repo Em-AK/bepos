5c3fac418213b9a255f9533a96df85c1ef3fcd29
em-ak <emilien.ak@gmail.com>
2013-09-10 12:27:57 +0200

    Add reading time feature
     + display time(in minutes) to read an article

--- lib/default.rb
+++ lib/default.rb
@@ -10,6 +10,17 @@ module PostHelper
   def get_pretty_date(post)
     attribute_to_time(post[:created_at]).strftime('%B %-d, %Y')
   end
+  def get_reading_time(post)
+  	words_per_minute = 150.0
+  	time = post.raw_content.split.length / words_per_minute
+  	reading_time = "#{time.round} minutes"
+  	if time < 1
+  		reading_time = "< 1 minute"
+  	elsif time >= 1 && time < 1.6
+  		reading_time = "1 minute"
+  	end
+  	reading_time
+	end
 end
 
 include PostHelper
