05fd6143f12322f72de7e0bb68d97998e56ae907
emak <emilien.ak@gmail.com>
2015-08-01 19:02:09 +0200

    Dépoussiérage

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'nanoc', '~> 3.6.4'
+gem 'adsf'
+gem 'kramdown'
+gem 'builder'
--- lib/default.rb
+++ lib/default.rb
@@ -7,23 +7,26 @@ include Nanoc3::Helpers::LinkTo
 
 module PostHelper
 
-	# Human readable date from timestamp
+  WORDS_PER_MINUTE = 300.0
+
   def get_pretty_date(post)
     attribute_to_time(post[:created_at]).strftime('%B %-d, %Y')
   end
 
-  # Estimation of the time to read a post
+  def short_date(post)
+    attribute_to_time(post[:created_at]).strftime('%m/%Y')
+  end
+
   def get_reading_time(post)
-  	words_per_minute = 170.0
-  	time = post.raw_content.split.length / words_per_minute
-  	reading_time = "~ #{time.round}"
-  	if time < 1
-  		reading_time = "< 1"
-  	elsif time >= 1 && time < 1.6
-  		reading_time = "~ 1"
-  	end
-  	reading_time
-	end
+    minutes = post.raw_content.split.length / WORDS_PER_MINUTE
+    if minutes < 1
+      "< 1 min"
+    elsif minutes >= 1 && minutes < 1.6
+      "~ 1 min"
+    else
+      "~ #{minutes.round} min"
+    end
+  end
 end
 
 include PostHelper
